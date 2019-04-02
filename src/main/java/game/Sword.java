7f2d54e0e0f46d915f9298d512b77b836ed0e847
dipina <dipina@gmail.com>
2019-04-02 12:43:08 +0200

    new changes

---
+++ src/main/java/game/Sword.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Sword implements WeaponBehaviour {
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println ("A very dull knife.");
+
+	}
+
+	@Override
+	public void useWeapon() {
+		// TODO Auto-generated method stub
+		System.out.println("You slash madly with your sword.");
+	}
+}
+
